e41f816004ce500b005e91495ef10c9e0306e0d2
Wuhealthy <1554531799@qq.com>
2026-08-17 16:33:29 +0800

    新增生物机甲特性

--- include/abilities.h
+++ include/abilities.h
@@ -346,5 +346,6 @@
 #define ABILITY_EEVEEHERO                            340    //全知全能
 #define ABILITY_FLOWERBLADE                          341    //花刃
 #define ABILITY_GRIDBIND                             342    //不稳定的电流
+#define ABILITY_BIOSHELL                             343    //生物机甲
 
-#define ABILITIES_COUNT                              343
+#define ABILITIES_COUNT                              344
--- scripts/build.py
+++ scripts/build.py
@@ -150,6 +150,7 @@ CANONICAL_U16_ABILITIES = {
     'ABILITY_EEVEEHERO',
     'ABILITY_FLOWERBLADE',
     'ABILITY_GRIDBIND',
+    'ABILITY_BIOSHELL',
 }
 
 # Species whose old DPE records used an effect-sharing byte instead of their
